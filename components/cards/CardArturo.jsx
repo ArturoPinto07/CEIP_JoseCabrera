742ab768d7d932a57a8374f1a2b06b5996dbe3bb
ArturoPinto07 <arturojopc@gmail.com>
2026-05-20 13:38:40 +0200

    feat: añadir componente CardArturo

---
+++ components/cards/CardArturo.jsx
@@ -0,0 +1,15 @@
+export default function CardArturo() {
+  return (
+    <div className="tarjeta">
+      <span className="avatar">🧑‍🏫</span>
+      <h3>Arturo</h3>
+      <p className="rol">Web Developer</p>
+      <p className="bio">Pensador divergente</p>
+      <div className="tags">
+        <span>Git</span>
+        <span>GitHub</span>
+        <span>Next.js</span>
+      </div>
+    </div>
+  );
+}
